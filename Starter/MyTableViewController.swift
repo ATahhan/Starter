75c65d516588b1a87f87104a0eef93537fedea11
yarob55 <yarob.hm2@gmail.com>
2018-04-05 17:03:53 +0300

    Add MyTableViewController

---
+++ Starter/MyTableViewController.swift
@@ -0,0 +1,144 @@
+//
+//  MyTableViewController.swift
+//  Practice
+//
+//  Created by يعرب المصطفى on 4/3/18.
+//  Copyright © 2018 yarob. All rights reserved.
+//
+
+import UIKit
+
+class MyTableViewController: UITableViewController {
+
+    var posts = [Post]()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let baseUrl = "https://jsonplaceholder.typicode.com"
+        let postsUrl = "/posts"
+        
+        print("started")
+        let request = PostRequest()
+        request.request(url: baseUrl+postsUrl)
+        { (posts) in
+            self.posts = posts
+            self.tableView.reloadData()
+        }
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 200
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        //return posts.count
+        return 40
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath) as! CustomTableViewCell
+
+        // Configure the cell...
+        
+        //cell.setup(thePost:posts[indexPath.row])
+        
+        cell.setupColor(index: indexPath.row)
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath)
+    {
+        tableView.deselectRow(at: indexPath, animated: false)
+        let cell = tableView.cellForRow(at: indexPath) as! CustomTableViewCell
+        performSegue(withIdentifier: "toView", sender: cell)
+    }
+    
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let id = segue.identifier
+        {
+            if id == "toView"
+            {
+                if let vc = segue.destination as? NotPriceDetailsViewController
+                {
+                    if let cell = sender as? CustomTableViewCell
+                    {
+                        var index  = tableView.indexPath(for: cell)!
+                        
+                        vc.viewColor = Color.colors[index.row%Color.colors.count]
+                        
+                       
+                    }
+                    
+                }
+            }
+        }
+    }
+ 
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
